a4ec2a6bee497f3bc3e26a36e02713cdae9a91c0
Javier Couce <jcoucelanga@gmail.com>
2017-03-12 14:07:20 +0100

    Mensaje de products cambiado

--- rest/routes/api.js
+++ rest/routes/api.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 //Routes
 router.get('/products', function(req, res) {
-    res.send('<div style="background:red;"> Bienvenido al api rest muahhaah</div>');
+    res.send('<div style="background:red; text-align:center;"> Bienvenido al api rest muahhaah</div>');
 });
 
 //Return ruter
